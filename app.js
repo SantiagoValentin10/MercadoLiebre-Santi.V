2d9c5124ae08cfb278e8bcc9c56df42b9e484397
SantiagoValentin10 <santiagovalentin100@gmail.com>
2022-10-24 18:38:25 -0300

    cambios en app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.listen(3000, (req, res) => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server corriendo");
 });
 
